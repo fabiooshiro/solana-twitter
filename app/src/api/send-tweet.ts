915c660d7a134b5cc745532086b21c094ed1fc7f
Fabio Oshiro <fabio.oshiro@gmail.com>
2022-11-08 14:54:44 -0300

    feat: sent-tweet

--- app/src/api/send-tweet.ts
+++ app/src/api/send-tweet.ts
@@ -1,12 +1,21 @@
 import { web3 } from '@project-serum/anchor'
 import { useWorkspace } from '@/composables'
 import { Tweet } from '@/models'
+import { PublicKey } from '@solana/web3.js';
+
+const findTweetAddress = async (author: PublicKey, userTweetId: string, program: any) => {
+    const [publicKey, _bump2] = await PublicKey.findProgramAddress([
+        author.toBuffer(),
+        Buffer.from(userTweetId),
+    ], program.programId);
+    return { publicKey };
+}
 
 export const sendTweet = async (topic, content) => {
     const { wallet, program } = await useWorkspace()
-    const tweet = web3.Keypair.generate()
-
-    await program.rpc.sendTweet(topic, content, {
+    const userTweetId = Date.now().toString()
+    const tweet = await findTweetAddress(wallet.publicKey, userTweetId, program);
+    await program.rpc.sendTweet(topic, content, userTweetId, {
         accounts: {
             author: wallet.publicKey,
             tweet: tweet.publicKey,
